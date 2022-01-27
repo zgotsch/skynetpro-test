742286ec21a3ad26b425644591353d8aba93225f
Zach Gotsch <orbipedis@gmail.com>
2022-01-27 14:22:11 -0800

    Try to load mysky

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from "react";
+import "./App.css";
+
+import {SkynetClient} from "skynet-js";
+const skynetClient = new SkynetClient("https://skynetpro.net/");
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  useEffect(() => {
+    skynetClient.loadMySky("localhost").then(() => {
+      console.log("loaded mysky");
+    });
+  }, []);
+
+  return <div className="App">hello</div>;
 }
 
 export default App;
